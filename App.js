614515763979f17bf4eb2e4c623ac90395d9e375
Mitch Masia <mjmasia@gmail.com>
2017-12-17 16:01:19 -0500

    Added help text to master.

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#F5F7F9' }}>
+        <Text
+          style={{
+            marginTop: 200,
+            color: '#9d9d9d',
+            fontSize: 24,
+            textAlign: 'center',
+          }}
+        >
+          You know what to do :)
+        </Text>
       </View>
     )
   }
