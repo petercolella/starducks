c41e2dfb44c77e19684be80ced49f2152cf574b6
Mitch Masia <mjmasia@gmail.com>
2017-12-18 17:49:46 -0500

    Update menu items in master.

--- src/fixtures/menuItems.js
+++ src/fixtures/menuItems.js
@@ -2,26 +2,26 @@ export default [
   {
     id: 0,
     title: 'Pumpkin Spice Latte',
-    imageSource: 'https://static.pexels.com/photos/414630/pexels-photo-414630.jpeg',
+    imageSource: 'http://www.telegraph.co.uk/content/dam/food-and-drink/2017/01/04/coffeeS006G8_wwwAlamycom_Heart-shaped-coffee-art-on-a-latte_trans_NvBQzQNjv4BqEDjTm7JpzhSGR1_8ApEWQA1vLvhkMtVb21dMmpQBfEs.jpg?imwidth=450',
   },
   {
     id: 1,
     title: 'Eggnog Surprise',
-    imageSource: 'https://static.pexels.com/photos/434213/pexels-photo-434213.jpeg',
+    imageSource: 'https://s3-media4.fl.yelpcdn.com/bphoto/iLIu6hYh0Sx8kZmTEZeAZw/348s.jpg',
   },
   {
     id: 2,
     title: 'Watereddown Milk',
-    imageSource: 'http://cdn.cnn.com/cnnnext/dam/assets/150929101049-black-coffee-stock-super-tease.jpg',
+    imageSource: 'http://www.goodmorningcoffee.com/images/CoffeeCup.png',
   },
   {
     id: 3,
     title: 'Hot Chocolate Apple Juice',
-    imageSource: 'http://nationaldailyng.com/wp-content/uploads/2017/07/pp-hot-coffee-rf-istock.jpg',
+    imageSource: 'http://dlo9n43mpvj20faa12i3i3lh.wpengine.netdna-cdn.com/wp-content/uploads/2014/05/coffee-beans-300x180.jpg',
   },
   {
     id: 4,
     title: 'Butternut Squash Express',
-    imageSource: 'https://static.pexels.com/photos/347144/pexels-photo-347144.jpeg',
+    imageSource: 'http://listverse.wpengine.netdna-cdn.com/wp-content/uploads/2017/10/7b-black-coffee-heart-health-686190918.jpg',
   },
 ]
